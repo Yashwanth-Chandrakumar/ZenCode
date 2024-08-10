b71d34808ac864919d98d721f9de3612a82e0ed4
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-08-10 14:52:10 +0530

    Update ArenaPage.jsx

--- frontend/src/pages/ArenaPage.jsx
+++ frontend/src/pages/ArenaPage.jsx
@@ -11,13 +11,13 @@ function ArenaPage() {
 
   return (
     <div className="flex h-screen bg-gray-100">
-      {/* Sidebar */}
       <div
-        className={`fixed inset-y-0 left-0 z-30 w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out ${
+        className={`fixed inset-y-0 left-0 top-4 z-30 w-64 bg-white shadow-lg transform transition-transform duration-300 ease-in-out ${
           isSidebarOpen ? "translate-x-0" : "-translate-x-full"
         }`}
       >
-        <div className="flex items-center justify-between p-4 border-b">
+        <h2 className="text-black dark:text-white" >Problem List</h2>
+        <div className="flex items-center justify-between p-4">
           <button
             onClick={toggleSidebar}
             className="text-gray-500 hover:text-gray-700 focus:outline-none lg:hidden"
