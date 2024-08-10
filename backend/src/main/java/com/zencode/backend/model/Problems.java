b77e5522de106a7b23f94483ae9984bcd71c96cc
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-08-10 19:03:32 +0530

    Update Problems.java

--- backend/src/main/java/com/zencode/backend/model/Problems.java
+++ backend/src/main/java/com/zencode/backend/model/Problems.java
@@ -5,9 +5,6 @@ import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 
-<<<<<<< Updated upstream
-import jakarta.persistence.*;
-=======
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -16,7 +13,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
->>>>>>> Stashed changes
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
