ed5ccd622f0149fb9531bda17d0ae115fa91fb3f
Yashwanth-Chandrakumar <yashwanth2k05@gmail.com>
2024-08-10 15:01:01 +0530

    Update Problems.java

--- backend/src/main/java/com/zencode/backend/model/Problems.java
+++ backend/src/main/java/com/zencode/backend/model/Problems.java
@@ -1,9 +1,11 @@
 package com.zencode.backend.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,9 +14,6 @@ import jakarta.persistence.Table;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Entity
 @Data
