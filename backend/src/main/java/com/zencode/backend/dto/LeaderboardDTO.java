6a224db286c0d6179bd6dfa9aa59a06675a051b6
VIGNESH VASU <vigneshvasu0511@gmail.com>
2024-08-11 10:11:40 +0530

    update model

--- backend/src/main/java/com/zencode/backend/dto/LeaderboardDTO.java
+++ backend/src/main/java/com/zencode/backend/dto/LeaderboardDTO.java
@@ -6,6 +6,8 @@ import lombok.Data;
 public class LeaderboardDTO {
     private Long id;
     private String userName;
+    private String email;
+    private boolean hasParticipated;
     private int score;
     private String timeTaken;
     private Long contestId; 
